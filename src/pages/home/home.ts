ab7d699583acce3b02f8721d11e1154450c6e53b
Your Name <you@example.com>
2019-10-16 20:12:40 -0500

    Variable nombre enlazada al input

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,15 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  
+  nombre = '';
+  
   constructor(public navCtrl: NavController) {
 
+  }
+
+  click(){
+    console.log('Hola ', this.nombre);
   }
 
 }
